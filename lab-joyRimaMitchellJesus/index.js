5e93377c3bae5ffea7031a635bc2e76b3c528930
Mitchell M <mitchellm1618@gmail.com>
2018-01-18 15:05:50 -0800

    created dev branch, filled out bitmap.js file

--- lab-joyRimaMitchellJesus/index.js
+++ lab-joyRimaMitchellJesus/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const reader = require('./lib/reader');
+const transform = require('./lib/transform');
+const bitmap = require ('./lib/bitmap')
--- lab-joyRimaMitchellJesus/lib/reader.js
+++ lab-joyRimaMitchellJesus/lib/reader.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const reader = module.exports = {};
+
+reader.read = function() {
+  fs.readFile('./assets/bitmap.bmp', (err, data) => {
+    let bmp = new bmp(data);
+    console.log(bmp)
+
+    console.log(bmp.pixelArray.length);
+  })
+}
