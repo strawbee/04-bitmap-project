99747f0381897c1f1bb665bc20a5bcc46fff3d3a
Rima <rmhrok@gmail.com>
2018-01-21 06:01:02 +0000

    modularize and add two more test cases

--- lab-joyRimaMitchellJesus/lib/transform.js
+++ lab-joyRimaMitchellJesus/lib/transform.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+function invert(data) {
+  for (let i = 0; i < data.pixelArray.length; i++) {
+    data.pixelArray[i] = 255 - data.pixelArray[i];            
+  }
+
+  return data;
+}
+
+function greyscale(data) {
+  for (let i = 0; i < data.pixelArray.length / 4; i += 4) {
+    let grey = (data.pixelArray[i] + data.pixelArray[i + 1] +data.pixelArray[i + 2]) / 3;
+    data.pixelArray[i] = grey;
+    data.pixelArray[i + 1] = grey;
+    data.pixelArray[i + 2] = grey;
+  }
+  return data;
+}
+
+function randomize(data) {
+  for (let i = 0; i < data.pixelArray.length; i++) {
+    data.pixelArray[i] = Math.floor(Math.random() * Math.floor(255));
+  }
+  return data;
+}
+
+module.exports = {invert, greyscale, randomize};
