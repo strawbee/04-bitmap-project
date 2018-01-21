ebb71083561cf1c48f4b8ef1bf4c6374dd69db64
strawbee <yuejiao.hou@gmail.com>
2018-01-21 06:59:52 -0800

    CLI complete

--- lab-joyRimaMitchellJesus/index.js
+++ lab-joyRimaMitchellJesus/index.js
@@ -2,5 +2,21 @@
 
 const reader = require('./lib/reader');
 
-reader.read();
+console.log(`
+====================== BITMAP APP ======================
+            By: Joy, Rima, Mitchell, & Jesus
+               Code Fellows JavaScript 401
 
+                       [ HOW TO ] 
+1. From the lab-joyRimaMitchellJesus folder, 
+   run node in your terminal and enter the following line:
+    const reader = require('./lib/reader');
+
+2. Transform bitmap images using the following format:
+    reader.read(input-path, output-path, transform-name);
+
+   All arguments should be strings.
+
+              [ AVAILABLE TRANSFORM NAMES ] 
+              invert, greyscale, randomize
+`);
--- lab-joyRimaMitchellJesus/lib/transform.js
+++ lab-joyRimaMitchellJesus/lib/transform.js
@@ -11,7 +11,7 @@ function invert(data) {
 
 function greyscale(data) {
   for (let i = 0; i < data.pixelArray.length / 4; i += 4) {
-    let grey = (data.pixelArray[i] + data.pixelArray[i + 1] +data.pixelArray[i + 2]) / 3;
+    let grey = (data.pixelArray[i] + data.pixelArray[i + 1] + data.pixelArray[i + 2]) / 3;
     data.pixelArray[i] = grey;
     data.pixelArray[i + 1] = grey;
     data.pixelArray[i + 2] = grey;
@@ -21,7 +21,7 @@ function greyscale(data) {
 
 function randomize(data) {
   for (let i = 0; i < data.pixelArray.length; i++) {
-    data.pixelArray[i] = Math.floor(Math.random() * Math.floor(255));
+    data.pixelArray[i] = Math.floor(Math.random() * 255);
   }
   return data;
 }
