7eedeacfd6ae731f58d61d396b1a926ede454d86
Rima <rmhrok@gmail.com>
2018-01-22 07:14:20 +0000

    add another tets case in reader.test.js

--- lab-joyRimaMitchellJesus/lib/reader.js
+++ lab-joyRimaMitchellJesus/lib/reader.js
@@ -38,39 +38,9 @@ reader.read = (inputPath, outputPath, transformName) => {
     // write a new file
     fs.writeFileSync(outputPath, transformBmp.allData);
     console.log(`Your bitmap at ${inputPath} has been modified using our ${transformName} function. The modified bitmap has been written to ${outputPath}.`);
+    return 'SUCCESS: Transformed file created';
   } catch (err) {
     return 'ERROR: Invalid Output Path';
   }
 }
         
-      //console.log(`Your bitmap at ${inputPath} has been modified using our ${transformName} function. The modified bitmap has been written to ${outputPath}.`);
-/*    });
-  } catch (err) {
-    console.error(err);
-    return 'ERROR: Invalid Input Path';
-  }
-
-    // read BMP
-    let bmp = new bitmap.bitmap(data);
-
-    // setup and error validation for transform
-    let transformBmp;
-    if (transformName === 'invert' || transformName === 'inverse') transformBmp = trans.invert(bmp);
-    else if (transformName === 'greyscale' || transformName === 'grayscale') transformBmp = trans.greyscale(bmp);
-    else if (transformName === 'randomize' || transformName === 'random') transformBmp = trans.randomize(bmp);
-    else {
-      console.log('You have entered an invalid transform name. The parameters to this function are inputPath, outputPath, and transformName, where valid transform names are invert, greyscale, and randomize.');
-      return;
-    }
-
-    // transform BMP
-    fs.writeFile(outputPath, transformBmp.allData, (err) => {
-      if (err) {
-        console.error(err);
-        return 'ERROR: Invalid Output Path';
-      } 
-        
-      console.log(`Your bitmap at ${inputPath} has been modified using our ${transformName} function. The modified bitmap has been written to ${outputPath}.`);
-    });
-  });
-};*/
