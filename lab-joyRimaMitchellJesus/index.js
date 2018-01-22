c67ff35c0357e1c2ed8060a4b7c2c77867a3e2f6
Rima <rmhrok@gmail.com>
2018-01-22 03:28:36 +0000

    Modify CLI

--- lab-joyRimaMitchellJesus/index.js
+++ lab-joyRimaMitchellJesus/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const reader = require('./lib/reader');
-
+/*
 console.log(`
 ====================== BITMAP APP ======================
             By: Joy, Rima, Mitchell, & Jesus
@@ -20,3 +20,28 @@ console.log(`
               [ AVAILABLE TRANSFORM NAMES ] 
               invert, greyscale, randomize
 `);
+*/
+
+//#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var program = require('commander');
+
+program
+  .version('1.1.0')
+  .option('-i, --input <path>', 'Add a file path you want to transform')
+  .option('-o, --output <path>', 'Add a file path you want to output')
+  .option('-t, --transform <transform>', 'Add invert or greyscale or randomize')
+  .parse(process.argv);
+
+console.log('We are going to ');
+if (program.transform === 'invert') console.log('invert ');
+if (program.transform === 'greyscale') console.log('greyscale ');
+if (program.transform === 'randomize') console.log('randomize ');
+if (program.input) console.log(program.input);
+if (program.output) console.log(program.output);
+
+reader.read(program.input, program.output, program.transform);
